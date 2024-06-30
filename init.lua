845857fa7da524f24490747cfe0f3df3c53058b5
Simeon Steward <simeontsteward@gmail.com>
2024-06-30 11:31:33 -0500

    Add dvorak keymaps

--- init.lua
+++ init.lua
@@ -90,6 +90,20 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Keymaps for dvorak
+vim.api.nvim_set_keymap('', 'h', 'h', { noremap = true })
+vim.api.nvim_set_keymap('', 't', 'j', { noremap = true })
+vim.api.nvim_set_keymap('', 'n', 'k', { noremap = true })
+vim.api.nvim_set_keymap('', 's', 'l', { noremap = true })
+vim.api.nvim_set_keymap('', 'H', '0', { noremap = true })
+vim.api.nvim_set_keymap('', 'T', 'L', { noremap = true })
+vim.api.nvim_set_keymap('', 'N', 'H', { noremap = true })
+vim.api.nvim_set_keymap('', 'S', '$', { noremap = true })
+vim.api.nvim_set_keymap('', 'j', 'n', { noremap = true })
+vim.api.nvim_set_keymap('', 'J', 'N', { noremap = true })
+vim.api.nvim_set_keymap('', 'k', 's', { noremap = true })
+vim.api.nvim_set_keymap('', 'K', 'S', { noremap = true })
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
@@ -100,9 +114,7 @@ vim.g.have_nerd_font = false
 
 -- Make line numbers default
 vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -152,7 +164,7 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 99
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -186,9 +198,9 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --
 --  See `:help wincmd` for a list of all window commands
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<C-s>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-t>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-n>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
